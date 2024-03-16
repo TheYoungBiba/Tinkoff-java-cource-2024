8a3d3a904dff149d2fb05fd907c72983d15d6ffb
TheYoungBiba <89189450996@mail.ru>
2024-03-16 15:04:01 +0300

    Files copied from hw4 branch

---
+++ db/migrations/SQL/init_users_links.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset TheYoungBiba:1
+CREATE TABLE users_links(
+    user_id BIGINT NOT NULL,
+    link_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, link_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (link_id) REFERENCES links (id)
+);
